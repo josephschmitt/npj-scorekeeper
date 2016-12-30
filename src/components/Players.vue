95e31e52936436483ada8e409cf82de32ec3a968
Joseph Schmitt <me@joe.sh>
2016-12-30 17:27:33 -0500

    Implement player filtering with search bar

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -1,5 +1,6 @@
 <template lang="html">
   <div>
+    <input class="topcoat-search-input full" type="search" value="" placeholder="Filter players" v-model="search">
     <ul class="topcoat-list topcoat-list__container">
       <li class="topcoat-list__item" v-for="player in players()">{{player.name}}</li>
     </ul>
@@ -7,14 +8,32 @@
 </template>
 
 <script>
+  import {mapState} from 'vuex';
+
   export default {
+    data() {
+      return {
+        search: ''
+      };
+    },
     computed: mapState({
       allPlayers: function(state) {
-	return state.players;
+        return state.players;
       }
     }),
+    methods: {
+      players: function() {
+        return this.allPlayers.filter((player) => {
+          return player.name.toLowerCase().includes(this.search.toLowerCase());
+        });
+      }
+    }
   }
 </script>
 
 <style lang="css">
+  .topcoat-search-input {
+    margin: 0.5rem 0.75rem;
+    width: calc(100% - 0.75rem * 2);
+  }
 </style>
