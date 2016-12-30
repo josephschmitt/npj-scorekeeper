e7e7ceacf3e42f1e634e0717d48798d2d0d75fb4
Joe Schmitt <me@joe.sh>
2016-12-30 16:37:13 +0000

    Add basic vuex store

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import * as actions from './actions.js';
+import * as getters from './getters.js';
+
+import games from './modules/games.js';
+import players from './modules/players.js';
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  actions,
+  getters,
+  modules: {
+    games,
+    players
+  }
+})
